e4872ddf920c49299014268a6e50a0ec25a98a81
최수혁(choi soo hyeok) <121610467+choi55j@users.noreply.github.com>
2023-08-11 15:38:42 +0900

    Add files via upload

--- choi/php 코드/Q7-3-1.php
+++ choi/php 코드/Q7-3-1.php
@@ -2,22 +2,18 @@
     class Customer{
         public $name;
         public $address;
-        public $phone;
+        private $phone;
 
-        public function __construct($name,$address){
+        public function __construct($name,$address,$phone){
             $this->name = $name;
             $this->address = $address;
-        }
-        public function setPhone($phone){
-                $this->phone = $phone;
+            $this->phone = $phone;
         }
         public function getPhone(){
-                return $this->phone;
-        }
+            return $this->phone;        }
     }
 
-    $obj = new Customer("홍길동","수원");
-    $obj->phone="010-1234-5678";
+    $obj = new Customer("홍길동","수원","010-1234-5678");
 
     echo "이름 : ".$obj->name."<br>";
     echo "주소 : ".$obj->address."<br>";
