c2e91b26cdb60b47782c7285cfa9d180e42851be
최수혁(choi soo hyeok) <121610467+choi55j@users.noreply.github.com>
2023-09-04 11:48:55 +0900

    Add files via upload

---
+++ choi/mysql/get.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <h3>GET 방식</h3>
+    <a href="get_process.php?id=kim111&level=7">회원정보</a>
+</body>
+</html>
