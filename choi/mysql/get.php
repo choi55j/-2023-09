9873e3a8b110e7932270af80d43f513015b943f3
최수혁(choi soo hyeok) <121610467+choi55j@users.noreply.github.com>
2023-09-04 16:01:30 +0900

    Add files via upload

--- choi/mysql/get.php
+++ choi/mysql/get.php
@@ -1,13 +1,10 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+<meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="style.css">
 </head>
-<body>
+<body>    
     <h3>GET 방식</h3>
     <a href="get_process.php?id=kim111&level=7">회원정보</a>
 </body>
--- choi/mysql/get_process.php
+++ choi/mysql/get_process.php
@@ -2,7 +2,7 @@
     $id = $_GET["id"];
     $level = $_GET["level"];
 
-    echo "<h3>회원정보</h3>";
-    echo "-아이디 : $id<br>";
-    echo "-레벨 : $level";
+    echo "<h3>회원 정보</h3>";
+    echo "- 아이디 : $id<br>";
+    echo "- 레벨 : $level";
 ?>
